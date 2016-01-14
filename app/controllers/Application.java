6d5e8e8ab796f04635f77c95ca38f8b5be628408
Aman1301 <amanjot.singh@raremile.com>
2016-01-14 13:03:21 +0530

    Added

---
+++ app/controllers/Application.java
@@ -0,0 +1,85 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import play.*;
+import play.data.Form;
+import play.mvc.*;
+import views.html.*;
+import model.Ticket;
+import model.User;
+import model.service;
+
+public class Application extends Controller {
+
+	private  service s= null;
+	
+	public service getService(){
+		if(s==null){
+			s=  new service();
+		}
+		return s;
+	}
+	
+    public Result index() {
+        return ok(index.render("Your new application is ready."));
+    }
+
+    public Result sayHello() {
+    	User  u= Form.form(User.class).bindFromRequest().get();
+    	boolean status=getService().checkUser(u);
+    	System.out.println(status);
+    	if(status)
+        return ok(welcome.render(u.username));
+    	else {
+    	return ok(index.render("Invalid Username/Password"));
+		}
+    }
+    
+    public Result newTicketPage() {
+        return ok(raiseTickets.render(null,new Ticket()));
+    }
+    
+    public Result displayTickets() {
+    	List<Ticket> ticketsList= getService().getAllTickets();
+    	List<Ticket> ticketsListNew=new ArrayList<Ticket>();
+    	List<Ticket> ticketsListOpen=new ArrayList<Ticket>();
+    	List<Ticket> ticketsListClosed=new ArrayList<Ticket>();
+    	for(Ticket tck:ticketsList){
+    		if(tck.status.equals("New")){
+    			ticketsListNew.add(tck);
+    		}else if (tck.status.equals("Open")) {
+    			ticketsListOpen.add(tck);
+			}else if (tck.status.equals("Closed")) {
+				ticketsListClosed.add(tck);
+			}
+    		
+    	}
+        return ok(viewTicket.render(ticketsListNew,ticketsListOpen,ticketsListClosed));
+    }
+    
+    public Result newTicket() {
+    	Ticket  t= Form.form(Ticket.class).bindFromRequest().get();
+    	boolean status = getService().insertTicket(t);
+    	System.out.println(t.ticketId);
+    	System.out.println(t.customerInfo);
+    	System.out.println(t.createdBy);
+    	System.out.println("t.ticketId "+t.ticketId);
+        return ok(raiseTickets.render(null,new Ticket()));
+    }
+    
+    public Result editTicketStatus(String ticket_id) {
+    	Ticket tic=getService().getTicketDetail(ticket_id);
+    	Form<Ticket> ft= Form.form(Ticket.class).fill(tic);
+    	System.out.println(tic.comment);
+        return ok(updateTicket.render(ticket_id,ft));
+    }
+    public Result updateTicket(String ticket_id) {
+    	
+    	Ticket  t= Form.form(Ticket.class).bindFromRequest().get();
+    	
+    	boolean tics=getService().updateTicket(t);
+        return ok(welcome.render(""));
+    }
+}
---
+++ app/model/Ticket.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Ticket{
+
+	public String ticketId;
+	public String customerInfo;
+	public String comment;
+	public String createdBy;
+	public String assignedTo;
+	public String status;
+	
+	public String getTicketId() {
+		return ticketId;
+	}
+	public void setTicketId(String ticketId) {
+		this.ticketId = ticketId;
+	}
+	public String getCustomerInfo() {
+		return customerInfo;
+	}
+	public void setCustomerInfo(String customerInfo) {
+		this.customerInfo = customerInfo;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public String getAssignedTo() {
+		return assignedTo;
+	}
+	public void setAssignedTo(String assignedTo) {
+		this.assignedTo = assignedTo;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
